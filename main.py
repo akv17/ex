332f58b55893e011226acbd4a5cbc180ce8fbf9a
unknown <student@basm-509-std02.staff.corp.local>
2016-06-29 11:13:33 +0300

    fixed version of 1-2 tasks

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ def op():
     return f
 
 def tsk1(x):
+    print('Task 1\n')
     info = re.findall('[А-Я]\.\s?[А-Я]\w+', x)
     for el in info:
         print(el)
     
 def tsk2(y):
+    print('\nTask 2\n')
     info = re.findall('([А-Я]\.\s?[А-Я]\w+)|([А-Я]\.\s[А-Я]\.\s?[А-Я]\w+)|([А-Я]\w+ +[А-Я]\w+)', y)   
     for el in info:
         for match in el:
             if match != '':
                 print(match)
 
-tsk2(op())        
+def main():
+    val = tsk1(op())
+    val2 = tsk2(op()) 
+    return val, val2
+
+main()    
+    
     
